eea6f81e9e4525910b3ab6a0451b0e9d1756aac8
desol1997 <denys.soloviov.ext@fashion-digital.de>
2023-12-17 22:51:08 +0200

    put public URL into test file & add screenshot of deployment

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+url = 'https://sh1oyyzmr4.execute-api.us-east-1.amazonaws.com/test/predict'
 data = {'url': 'https://raw.githubusercontent.com/SartajBhuvaji/Brain-Tumor-Classification-DataSet/master/Testing/no_tumor/image(103).jpg'}
 
 result = requests.post(url=url, json=data).json()
